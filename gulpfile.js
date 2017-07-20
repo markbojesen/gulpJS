a052fb531941ef1b8e26978715d2f0aabe23bbee
Mark Bojesen <bojesenmark@gmail.com>
2017-07-20 21:24:21 +0100

    add uglify and sass to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@
 // Requirements
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
 
 // Copy HTML files from SRC to DIST 
 gulp.task('copyHtml', function() {
@@ -19,4 +21,18 @@ gulp.task('imageMin', () => {
     gulp.src('src/images/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/images'));
-})
+});
+
+// Minify JS
+gulp.task('minify', function(){
+    gulp.src('src/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+}); 
+
+// Compile Sass
+gulp.task('sass', function() {
+    gulp.src('src/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+});
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+
+// This should be minified and uglified :)
+console.log('This should be minified and uglified!')
